0bf228d3807fd8b1a5ab490c8c3b3706f9e56505
Ваше Имя <.email@example.com>
2026-05-02 15:58:08 +0600

    type: fix

--- AUZ/settings.py
+++ AUZ/settings.py
@@ -149,7 +149,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://schooldiaryw.netlify.app",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
